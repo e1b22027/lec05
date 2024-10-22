089264879686fef6e790dbcacdefc7f46af05f34
Halto709 <e1b22071@oit.ac.jp>
2024-10-22 14:47:49 +0900

    購入処理（選択した果物をテーブルから削除し，何を購入したかを表示する）

--- src/main/java/oit/is/lec05/kadai/lec05/controller/Sample51Controller.java
+++ src/main/java/oit/is/lec05/kadai/lec05/controller/Sample51Controller.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.lec05.kadai.lec05.model.Fruit;
 import oit.is.lec05.kadai.lec05.model.FruitMapper;
@@ -28,6 +30,22 @@ public class Sample51Controller {
 
   @GetMapping("step2")
   public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  @GetMapping("step3")
+  @Transactional
+  public String sample53(@RequestParam Integer id, ModelMap model) {
+    // 削除対象のフルーツを取得
+    Fruit fruit3 = fMapper.selectById(id);
+    model.addAttribute("fruit3", fruit3);
+
+    // 削除
+    fMapper.deleteById(id);
+
+    // 削除後のフルーツリストを取得
     ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
     model.addAttribute("fruits2", fruits2);
     return "sample51.html";
