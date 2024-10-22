53057d05155ce1166f43ad25e068e41f49bca2c8
e1b22025 <e1b22025@oit.ac.jp>
2024-10-22 14:00:49 +0900

    認証処理を実装する

---
+++ src/main/java/security/Sample3AuthConfiguration.java
@@ -0,0 +1,44 @@
+package oit.is.lec05.kadai.lec05.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration {
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   *
+   * @return
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+
+    UserDetails customer1 = User.withUsername("customer1")
+        .password("{bcrypt}$2y$05$hQz59KuMIFZ6n8iIhG8Zo.xv29GbxNnB2XTjw4axe0lPVjVPokDh2").roles("CUSTOMER").build();
+    UserDetails customer2 = User.withUsername("customer2")
+        .password("{bcrypt}$2y$05$hQz59KuMIFZ6n8iIhG8Zo.xv29GbxNnB2XTjw4axe0lPVjVPokDh2").roles("CUSTOMER").build();
+    UserDetails seller = User.withUsername("seller")
+        .password("{bcrypt}$2y$05$hQz59KuMIFZ6n8iIhG8Zo.xv29GbxNnB2XTjw4axe0lPVjVPokDh2").roles("SELLER").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(customer1, customer2, seller);
+  }
+
+}
