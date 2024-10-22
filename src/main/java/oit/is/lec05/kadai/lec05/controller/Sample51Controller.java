fe63f4af98e51d3b91aec9c790668ec660ffd3a5
e1b22063 <e1b22063@oit.ac.jp>
2024-10-22 14:35:25 +0900

    すべての果物のレコードを表示する

--- src/main/java/oit/is/lec05/kadai/lec05/controller/Sample51Controller.java
+++ src/main/java/oit/is/lec05/kadai/lec05/controller/Sample51Controller.java
@@ -1,8 +1,15 @@
 package oit.is.lec05.kadai.lec05.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.lec05.kadai.lec05.model.Fruit;
+import oit.is.lec05.kadai.lec05.model.FruitMapper;
 
 /**
  * /sample5へのリクエストを扱うクラス authenticateの設定をしていれば， /sample5へのアクセスはすべて認証が必要になる
@@ -10,8 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sample5")
 public class Sample51Controller {
+
+  @Autowired
+  FruitMapper fMapper;
+
   @GetMapping("step1")
   public String sample51() {
+    return "sample51.html";
+  }
+
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
     return "sample51.html";
   }
 }
---
+++ src/main/java/oit/is/lec05/kadai/lec05/model/FruitMapper.java
@@ -0,0 +1,15 @@
+package oit.is.lec05.kadai.lec05.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface FruitMapper {
+
+  @Select("SELECT ID, NAME,PRICE FROM FRUIT")
+  ArrayList<Fruit> selectAllFruit();
+}
