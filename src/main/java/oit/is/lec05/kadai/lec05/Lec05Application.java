33a32ff018889455f9d3442e4bf8415caa2dd3b8
e1b22027 <e1b22027@oit.ac.jp>
2024-10-22 15:57:32 +0900

    ブラウザで指定のページを開いている限りカウントアップする数字を表示する処理を実装

--- src/main/java/oit/is/lec05/kadai/lec05/Lec05Application.java
+++ src/main/java/oit/is/lec05/kadai/lec05/Lec05Application.java
@@ -2,12 +2,14 @@ package oit.is.lec05.kadai.lec05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class Lec05Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lec05Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Lec05Application.class, args);
+  }
 
 }
---
+++ src/main/java/oit/is/lec05/kadai/lec05/service/AsyncCountFruit56.java
@@ -0,0 +1,34 @@
+package oit.is.lec05.kadai.lec05.service;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+public class AsyncCountFruit56 {
+  int count = 1;
+  private final Logger logger = LoggerFactory.getLogger(AsyncCountFruit56.class);
+
+  @Async
+  public void count(SseEmitter emitter) throws IOException {
+    logger.info("count start");
+    try {
+      while (true) {// 無限ループ
+        logger.info("send:" + count);
+        // sendによってcountがブラウザにpushされる
+        emitter.send(count);
+        count++;
+        // 1秒STOP
+        TimeUnit.SECONDS.sleep(1);
+      }
+    } catch (InterruptedException e) {
+      // 例外の名前とメッセージだけ表示する
+      logger.warn("Exception:" + e.getClass().getName() + ":" + e.getMessage());
+    }
+  }
+}
